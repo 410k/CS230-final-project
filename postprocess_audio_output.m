34a477ef2f8532116f3c881b75f6d2c6addb02ba
Steve Leung <stevealeung@gmail.com>
2018-02-25 17:42:37 -0800

    commented the postprocess and data reformat code

--- postprocess_audio_output.m
+++ postprocess_audio_output.m
@@ -1,14 +1,16 @@
+%% visualize the outputs
 m = reshape(permute(model_output,[3,2,1]), [], 1);
 t = reshape(permute(true_output,[3,2,1]), [], 1);
 figure; hold on;
 plot(m);
 plot(t);
 legend('True audio', 'Predicted audio');
+xlabel('Sample #');
+ylabel('Amplitude');
 
+
+%% fourier transform the audio
 fs = 44100;
-% sound(m, fs);
-
-
 sampling_period = 1/fs;
 window_length = length(t) * sampling_period;
 freq_extent = fs;
@@ -17,6 +19,8 @@ freqs = (-fs/2:freq_interval:fs/2-freq_interval)';
 
 M = fftshift(fft(fftshift(m)));
 
+
+%% bandpass filter the audio
 freq_cutoff_low = 25;       % Hz
 freq_cutoff_high = 4200;    % Hz
 unwanted_freq_inds = abs(freqs) < freq_cutoff_low | freq_cutoff_high < abs(freqs);
@@ -33,6 +37,12 @@ xlabel('Frequency (Hz)');
 ylabel('Log(abs(amplitude))');
 grid on;
 
+
+%% fourier transform back to time domain
 m2 = real(ifftshift(ifft(ifftshift(M2))));
+
+
+%% listen to the audio
+% sound(m, fs);
 sound(m2, fs);
  
--- reformat_input_output_data.m
+++ reformat_input_output_data.m
@@ -3,16 +3,20 @@ filepath = 'data/train';
 target_file = 'ex15.mat';
 load(fullfile(filepath, target_file));
 
+segment_length = 100;
+
+
+%% pad the input & output data
 input = Xin;
 output = Yout;
-
 num_time_windows = size(input,1);
-segment_length = 100;
 pad_amount = segment_length - mod(num_time_windows, segment_length);
 input = padarray(input, pad_amount, 0, 'post');
 output = padarray(output, pad_amount, 0, 'post');
-num_segments = ceil(size(input,1) / segment_length);
 
+
+%% save every segment in its own file
+num_segments = ceil(size(input,1) / segment_length);
 for i = 1:num_segments
     inds = (i-1)*segment_length+1:i*segment_length;
     Xin = input(inds, :);
