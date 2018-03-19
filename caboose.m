5aac4469739e9ee9cb60713cb3f47d787509d72a
Steve Leung <stevealeung@gmail.com>
2018-03-18 22:32:23 -0700

    caboose.m now generates input data with time_window_duration = 5ms

--- caboose.m
+++ caboose.m
@@ -1,64 +1,57 @@
-% data read in
-tic
-d = dir('/Users/granty/Documents/CS230/mp3/');
-for ii = 4:length(d)
-    
-    %midi = readmidi('/Users/granty/Documents/CS230/midi/classical/Adagio in B flat.mid');
-     midi = readmidi(['/Users/granty/Documents/CS230/midi/classical/' d(ii).name(1:end-4) '.mid']);
-    %[aud,fs] = audioread('/Users/granty/Documents/CS230/mp3/Adagio in B flat.mp3');
-    [aud,fs] = audioread(['/Users/granty/Documents/CS230/mp3/' d(ii).name ]);
-    aud = decimate(aud,4)';
-    fs = fs/4;
-    [Notes, endtime] = midiInfo(midi,1,1);
-    dt = 0.01; %seconds
-    
-    % PR(midi note, time), t = time index (s), nn = midi note index
-    [PR,t,nn] = piano_roll(Notes,0,dt);
-    
-    % find note start indices (Articulated)
-    attack = conv2(PR,[1,-1]);
-    attack = attack(:,1:end-1);
-    attack( attack > 0 ) = 1;
-    attack( attack <= 0 ) = 0;
-    sustain = zeros(size(PR));
-    sustain( PR > 0 ) = 1;
-    
-    % input matrix ( sample, time, features)
-    time = 1; % time duration in seconds
-    N = 1/dt; % number of data points in time
- 
-    % Reshape audio segments
-    % crop audio to same size as Xin
-    Na = fs/time; % number of audio samples in time duration
-    num_samples = ceil(length(aud)/Na);
-    y = zeros(num_samples*Na,1);
-    y(1:length(aud)) = aud;
-    Yout = reshape(y,Na,num_samples)'; %output matrix
-    
-    time_audio = length(y)/fs;
-    
-    % input matrix (time, pitch (keys 1-88 on the piano)
-    Xin = zeros(size(sustain,2),176);
-    for i = 1:length(nn)
-        Xin(:,(nn(i)-20)*2-1) = attack(i,:)'; % input articulation into odd columns
-        Xin(:,(nn(i)-20)*2) = sustain(i,:)'; % input sustain into even columns
-    end
-    
-    
-    % crop Xin
-    X = zeros(num_samples*N,176); % zero pad last sample
-    X(1:size(Xin,1),:) = Xin;
-    X = permute(reshape(X',176,N,num_samples),[3,2,1]);
-%     
+% script to resample midi files to different time window durations
 
+% setup
+time_window_duration = 0.005;           % seconds
+filename_extension = sprintf('_dt%02d', time_window_duration*1e3);
+save_dir = filename_extension;
+mkdir(save_dir);
+
+midi_dir = fullfile('TPD', 'classical');
+midi_listing = dir(fullfile(midi_dir, '*.mid'));
+
+keyboard_lowest_midi_note = 21;
+num_keyboard_keys = 88;
+
+tic;
+for n = 1:length(midi_listing)
+    % load the standard midi file
+    midi_filename = midi_listing(n).name;
+    fprintf('processing %s\n', midi_filename);
+    midi_filepath = fullfile(midi_dir, midi_filename);
+    midi = readmidi(midi_filepath);
+
+    % parse the midi and output a matrix indicating when each note is on
+    [notes, end_time] = midiInfo(midi, 0, 1);
+    [piano_notes_on, time_steps, midi_notes] = piano_roll(notes, 0, time_window_duration);
+    keyboard_keys = midi_notes - keyboard_lowest_midi_note + 1;
+    % piano_notes_on: (midi_notes, time_steps)
+    % time_steps: seconds
+    % keyboard_keys: array of keyboard keys played
+
+    % find note start indices (articulated)
+    articulate = conv2(piano_notes_on, [1,-1]);
+    articulate = articulate(:,1:end-1);
+    articulate( articulate > 0 ) = 1;
+    articulate( articulate <= 0 ) = 0;
+    sustain = zeros(size(piano_notes_on));
+    sustain( piano_notes_on > 0 ) = 1;
     
-%     test = pianoRoll2matrix(PR,dt,nn);
-%     y = midi2audio(matrix2midi(test),fs);
-%     sound(y,fs)
-%     
-%     test = Yout';
-%     sound(test(:),fs)
-    save(['/Users/granty/Data/CS230_train/keras_ex' num2str(ii) '.mat'],'X','Yout');
-    
+    % create the input matrix [time_windows, pitch_encoding (articulate & sustain)*88 keys on piano]
+    Xin = zeros(size(sustain,2), num_keyboard_keys*2);
+    for i = 1:num_keyboard_keys
+        sustain_index = i*2;
+        articulate_index = sustain_index-1;
+        keyboard_key_index = find(ismember(keyboard_keys, i));
+        if isempty(keyboard_key_index)
+            continue;
+        end
+        Xin(:, articulate_index) = articulate(keyboard_key_index,:)';   % input articulation into odd columns
+        Xin(:, sustain_index) = sustain(keyboard_key_index,:)';         % input sustain into even columns
+    end
+
+    % save the files
+    save_filename = sprintf('%s%s.mat', midi_filename(1:end-4), filename_extension);
+    save_filepath = fullfile(save_dir, save_filename);
+    save(save_filepath, 'Xin');
 end
-toc
+toc;
