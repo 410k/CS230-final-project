635221198042df8f10b8e9a6f037cbd36ee5496f
Steve Leung <stevealeung@gmail.com>
2018-03-19 00:55:59 -0700

    added loss scaling

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ parser.add_argument("-do", "--dropout",
 parser.add_argument("-llo", "--layer_lock_out",
                     type=str, default = None,
                     help="Layer Lock Out --> type layers in a delimited list, e.g. \"1,2\"")
+parser.add_argument("-sl", "--scale_loss",
+                    type=float, default=1,
+                    help="Scale loss by factor of sl")
 # input data options
 parser.add_argument("-nsongs", "--num_songs",
                     type=int, default=349,
@@ -104,7 +107,8 @@ dirs = setup_dirs(args)
 run_details_file = os.path.join(dirs['current_run'], 'run_details.txt')
 
 # architecture options
-architecture_options = ['hidden_units', 'layers', 'unidirectional', 'cell_type', 'batch_norm', 'dropout', 'layer_lock_out']
+architecture_options = ['hidden_units', 'layers', 'unidirectional', 'cell_type', 
+                        'batch_norm', 'dropout', 'layer_lock_out', 'scale_loss']
 print('architecture options')
 print('--------------------')
 with open(run_details_file,'a') as file:
@@ -175,7 +179,7 @@ from MidiNet import MidiNet
 from sklearn.model_selection import train_test_split
 
 import scipy.io
-from util import setup_dirs, load_data, save_predictions, split_data, save_audio
+from util import setup_dirs, load_data, save_predictions, split_data, save_audio, scaled_mse_functor
 from keras.callbacks import ModelCheckpoint, CSVLogger
 from keras.models import load_model
 import pandas as pd
@@ -245,7 +249,11 @@ def main():
                             unidirectional_flag, cell_type, batch_norm_flag, dropout, layer_lock_out_mask)
         if gpus >= 2:
             model = multi_gpu_model(model, gpus=gpus)
-        model.compile(loss='mean_squared_error', optimizer='adam')
+        if args.scale_loss != 1:
+            scaled_mse_loss = scaled_mse_functor(args.scale_loss)
+            model.compile(loss=scaled_mse_loss, optimizer='adam')
+        else:
+            model.compile(loss='mean_squared_error', optimizer='adam')
     print(model.summary())
 
     if args.load_last: # load last set of weights
